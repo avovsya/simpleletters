6cacca2dda7407220aa14fb7212caec908b0c96b
Artem Vovsya <avovsya@gmail.com>
2015-01-04 11:58:51 +0000

    Authorise user with Google account with permission to view email

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var config = require('config');
 
 var app = express();
 
@@ -20,10 +21,14 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: config.sessionSecret,
+  resave: false,
+  saveUninitialized: true
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var gmail = require('../lib/gmail');
+
 
 /* GET home page. */
 router.get('/', function(req, res) {
+  if (!req.session.user) {
+    console.log(req.session.user);
+    return res.redirect('auth');
+  }
   res.render('index', { title: 'Express' });
+});
+
+router.get('/auth', function (req, res) {
+  if (req.session.user) {
+    return res.redirect('/');
+  }
+  res.render('auth', {
+    authUrl: gmail.authUrl
+  });
+});
+
+router.get('/api/auth/callback', gmail.authCallbackHandler, function (req, res) {
+  res.redirect('/');
+  // Set user email to session variable
 });
 
 module.exports = router;
