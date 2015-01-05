03064ee04d74e14553f3af5480a5575309fa358b
Artem Vovsya <avovsya@gmail.com>
2015-01-05 20:37:53 +0000

    Show individual Emails

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,24 @@
 var express = require('express');
 var router = express.Router();
 var gmail = require('../lib/gmail');
-
+var authorize = require('../lib/auth').authorize;
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  if (!req.session.user) {
-    return res.redirect('auth');
-  }
+router.get('/', authorize, function(req, res) {
   gmail.getNewsletters(function (err, result) {
     if (err) {
-      //TODO: handle error
       res.send(500, err);
     }
     res.render('index', { letters: result });
+  });
+});
+
+router.get('/mail/:mailId', authorize, function (req, res) {
+  gmail.getMessage(req.params.mailId, function (err, result) {
+    if (err) {
+      res.send(500, err);
+    }
+    res.send(result);
   });
 });
 
