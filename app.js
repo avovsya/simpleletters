72fb93b51b85294fe5eaa1ce7a39a290b0a112f8
Artem Vovsya <avovsya@gmail.com>
2015-01-06 07:35:23 +0000

    Connect to MongoDB on start

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var store = require('./lib/store');
 
 var routes = require('./routes/index');
 var config = require('config');
