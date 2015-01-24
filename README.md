b7f8edc9ea3f7939fcb7c826006dd35d34bd1fc5
Artem Vovsya <avovsya@gmail.com>
2015-01-24 10:39:53 +0000

    Disable InstanClick in toolbar Move sensetive config to environment variables

---
+++ README.md
@@ -0,0 +1,9 @@
+## Environment variables
+
+SESSION_SECRET=[session secret value]
+
+MONGO_URL=[MongoDB connection string]
+
+GOOGLE_ID=[Google API app Id]
+GOOGLE_SECRET=[Google API app Secret]
+GOOGLE_CALLBACK=[Google API app callback URL]
--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(session({
-  secret: config.sessionSecret,
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }));
